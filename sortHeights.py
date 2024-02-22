67e6dadff5cba73798ef382e59ee21ed6a95c94a
egrizq <rizq.ramadhan17@gmail.com>
2024-02-22 20:23:15 +0700

    added 1

---
+++ sortHeights.py
@@ -0,0 +1,39 @@
+'''
+1051. Height Checker
+
+Example 1:
+Input: heights = [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+heights:  [1,1,4,2,1,3]
+expected: [1,1,1,2,3,4]
+Indices 2, 4, and 5 do not match.
+
+Example 2:
+Input: heights = [5,1,2,3,4]
+Output: 5
+Explanation:
+heights:  [5,1,2,3,4]
+expected: [1,2,3,4,5]
+All indices do not match.
+
+Example 3:
+Input: heights = [1,2,3,4,5]
+Output: 0
+Explanation:
+heights:  [1,2,3,4,5]
+expected: [1,2,3,4,5]
+All indices match.
+
+'''
+
+def heightChecker(heights) -> int:
+    sort = sorted(heights)
+    
+    counter = 0
+    for i in range(len(heights)):
+        if sort[i] != heights[i]:
+            counter += 1
+    return counter
+
+print(heightChecker([3,4,2,6]))
